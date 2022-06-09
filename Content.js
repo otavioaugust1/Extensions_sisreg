199ec46738f39f57de8cc26b2a49de01c11bc09d
Otavio Augusto <otavioaugust@gmail.com>
2022-06-09 13:17:28 -0300

    version 1.2

--- Content.js
+++ Content.js
@@ -1,35 +1,36 @@
 // --------- DECLARAÇÃO DE CONSTANCIAS BARRAS ---------//
 
-const interval = setInterval(()=>{
-    const barraMenu = document.querySelector("#barraMenu");
-    const barraSup = document.querySelector("#barraSistemaSup");
+const INTERNAL = setInterval(()=>{
+    const BARRA_MENU = document.querySelector("#barraMenu");
+    const BARRA_SUP = document.querySelector("#barraSistemaSup");
     
 //--------------- BARRA SUPERIOR----------------------//
 
-    if (barraMenu){
-        clearInterval(interval);
-        barraSup.innerHTML = "CANCELAMENTO AUTOMÁTICO DE SOLICITAÇÕES EM FILA";
-        barraSup.style.color = "#F5F5F5";
-        barraSup.style.background = "#64DD17"
-        barraSup.style.fontSize = "1.6rem";
-        barraSup.style.border = "none";
+    if (BARRA_MENU){
+        clearInterval(INTERNAL);
+        BARRA_SUP.innerHTML = "CANCELAMENTO AUTOMÁTICO DE SOLICITAÇÕES EM FILA";
+        BARRA_SUP.style.color = "#F5F5F5";
+        BARRA_SUP.style.background = "#64DD17"
+        BARRA_SUP.style.fontSize = "1.6rem";
+        BARRA_SUP.style.
+        BARRA_SUP.style.border = "none";
 
 // --------- DECLARAÇÃO DE MENU ----------------------//        
 
-        const menuCancelamento = document.createElement("a");
-        menuCancelamento.innerHTML = "CANCELAR AUTOMATICAMENTE ";
-        menuCancelamento.style.color = "#F5F5F5";
-        menuCancelamento.classList.add("cancelandoDevolvidosNx");
+        const MENU_CANCELAMENTO = document.createElement("a");
+        MENU_CANCELAMENTO.innerHTML = "CANCELAR AUTOMATICAMENTE ";
+        MENU_CANCELAMENTO.style.color = "#F5F5F5";
+        MENU_CANCELAMENTO.classList.add("cancelandoDevolvidosNx");
 
-        const barraProgresso = document.createElement("progress"); 
-        barraProgresso.classList.add("barraProgressoNx");
+        const BARRA_PROGRESSO = document.createElement("progress"); 
+        BARRA_PROGRESSO.classList.add("barraProgressoNx");
 
-        barraMenu.appendChild(menuCancelamento);
-        barraMenu.appendChild(barraProgresso);
+        BARRA_MENU.appendChild(MENU_CANCELAMENTO);
+        BARRA_MENU.appendChild(BARRA_PROGRESSO);
 
 // --------- DECLARAÇÃO DO CANCELAMENTO --------------// 
 
-        menuCancelamento.addEventListener("click", () => {
+        MENU_CANCELAMENTO.addEventListener("click", () => {
             let qtdeCancelamentos = prompt("Tenho "+listaCodSol.length+" solicitações para cancelar. Quantas devo cancelar? Informe a quantidade.");
             if (qtdeCancelamentos > listaCodSol.length || qtdeCancelamentos <= 0){
                 alert("Quantidade digitada não pode ser maior que o total apresentado a você. E nem zero ou menor que zero.");
@@ -38,12 +39,13 @@ const interval = setInterval(()=>{
                 let numFinal =  qtdeCancelamentos;
                 if (window.confirm("Tem certeza que devo cancelar "+ numFinal+" solicitações?")){
                     let index = 0;
-                    barraProgresso.max = numFinal;
-                    const tempoRestante = setInterval(()=>{
+                    BARRA_PROGRESSO.max = numFinal;
+                    const TEMPO_RESTANTE = setInterval(()=>{
 
 // -- DECLARAÇÃO MENSAGEM COLOCADA NO CANCELAMENTO ---// 
+                        let justifica = "Devido ao tempo decorrido sem reenvio/reavaliação/resposta a orientação registrada pelo profissional Regulador, a Gerência de Regulação Ambulatorial realiza o cancelamento administrativo. Orientamos que caso o paciente ainda necessite e procure a Unidade de Saúde em busca de atendimento, uma nova solicitação deverá ser inserida seguindo a orientação do Regulador e protocolo de acesso vigente.";
                         
-                        let url = "";
+                        let url = "https://sisregiiitreinamento.saude.gov.br/cgi-bin/cons_verificar?etapa=CANCELAR_SOLICITACAO&co_seq_solicitacao="+listaCodSol[index]+"&st_situacao=C&"+"ds_observacao="+justifica;                     
 
 // -- DECLARAÇÃO DA QUANT DE SOLICITACAO CANCELADA ---// 
 
@@ -52,16 +54,16 @@ const interval = setInterval(()=>{
                         siteSisreg.send();
 
                         index=index+1;
-                        barraProgresso.value=index;
-                        barraSup.style.color = "#F5F5F5";
-                        barraSup.style.fontSize = "1.6rem";
-                        barraSup.innerHTML = "Ultimo código cancelado: "+ (listaCodSol[index-1]) +" /  "+index+" de  "+numFinal;
+                        BARRA_PROGRESSO.value=index;
+                        BARRA_SUP.style.color = "#F5F5F5";
+                        BARRA_SUP.style.fontSize = "1.6rem";
+                        BARRA_SUP.innerHTML = "Ultimo código cancelado: "+ (listaCodSol[index-1]) +" /  "+index+" de  "+numFinal;
 
 // ---------- MENSAGEM DE FINALIZAÇÃO ----------------//
 
                         if (index == numFinal) {
-                            barraSup.innerHTML = "Ultimo código cancelado: "+ (listaCodSol[index-1]) +" /  "+index+" de  "+numFinal+"TERMINADO!!!";
-                            clearInterval(tempoRestante);
+                            BARRA_SUP.innerHTML = "Ultimo código cancelado: "+ (listaCodSol[index-1]) +" /  "+index+" de  "+numFinal+" TERMINADO!!!";
+                            clearInterval(TEMPO_RESTANTE);
                         }                                                
                     }, 3000); 
                 }else{
@@ -73,4 +75,6 @@ const interval = setInterval(()=>{
     }
 },2000);
 
-let listaCodSol = []
+// --------- LISTA DE SOLICITAÇÃO PARA CANCELAR ------//
+
+let listaCodSol = ["100072671"]
