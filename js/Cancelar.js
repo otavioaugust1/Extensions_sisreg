31e45c96a8896ebe1d32074e436c83a83852be04
Otavio Augusto <otavioaugust@gmail.com>
2022-06-23 16:18:56 -0300

    version 1.9.1

--- js/Cancelar.js
+++ js/Cancelar.js
@@ -1,9 +1,23 @@
 // --------- DECLARAÇÃO MENSAGEM ---------//
 
-var mensagemCancelamento = document.getElementById('mensagemCancelamento')
-var justifica = String(mensagemCancelamento)
 
 function clBtSalvarMsg(){
+    let mensJustifica = getElementById('mensagemCancelamento')
+    mensagemCancelamento.innerHTML = mensJustifica
+
+function clkBtPlay(){
     
+    let url = `https://sisregiiihomologacao.saude.gov.br/cgi-bin/gerenciador_solicitacao?etapa=CANCELAR_SOLICITACAO&co_seq_solicitacao=${listCodSol[index]}&st_situacao=C&ds_observacao=${mensJustifica}`
+
+    let siteSisreg = new XMLHttpRequest();
+    siteSisreg.open("GET", url, false);
+    siteSisreg.send();
+    index=index+1;
+
+
+
+
 }
+}
+
 
